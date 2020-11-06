52841b845c0ade8a72671ca3cc2985f8351a7aaa
serhiidernovyi <gifulmen@gmail.com>
2020-11-06 18:26:05 +0100

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NewsComponent} from './news/news.component';
+import {OneNewsComponent} from './one-news/one-news.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'news', component: NewsComponent},
+  {path: '', redirectTo: 'news', pathMatch: 'full'},
+  {path: 'one-news/:id', component: OneNewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NewsService} from './sevices/news.service';
+import {FormsModule} from '@angular/forms';
+import { OneNewsComponent } from './one-news/one-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    OneNewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NewsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
